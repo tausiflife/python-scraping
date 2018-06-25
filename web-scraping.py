f964acb5750509331e8198ba284295f7c3358a9e
tshak-master <tausif.056@gmail.com>
2018-06-25 21:29:22 +0200

    initial scraping code

---
+++ web-scraping.py
@@ -0,0 +1,28 @@
+from splinter import Browser
+import pandas as pd
+
+url = "https://www.google.com"
+
+browser = Browser('chrome')  # open a chrome browser
+browser.visit(url)  # goes to the url
+xpath = '//*[@id="lst-ib"]'
+
+search_bar = browser.find_by_xpath(xpath)[0]
+
+search_bar.fill("web scraping python")
+
+search_button_xpath = '//*[@id="tsf"]/div[2]/div[3]/center/input[1]'
+search_button = browser.find_by_xpath(search_button_xpath)[0]
+search_button.click()
+
+search_results_xpath = '//h3[@class="r"]/a'
+search_results = browser.find_by_xpath(search_results_xpath)
+
+scraped_data = []
+for search_result in search_results:
+    title = search_result.text.encode('utf8')  # trust me
+    link = search_result["href"]
+    scraped_data.append((title, link))
+
+df = pd.DataFrame(data=scraped_data, columns=["Title", "Link"])
+df.to_csv("links.csv")
